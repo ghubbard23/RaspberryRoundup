bc577d208cf76feabb5e07aa7f6abc5086d9bc57
ghubbard23 <ghubbard23@gmail.com>
2017-05-24 17:25:49 -0500

    Update RaspberryRoundup - Github.py

--- RaspberryRoundup - Github.py
+++ RaspberryRoundup - Github.py
@@ -6,26 +6,29 @@ import code128
 import datetime
 import os.path
 
-#time.sleep(60)
-
+#Check for ICMP connection to file server
 def ping():
     print('Checking for response from file server...')
     #ip of file server
     ip = "192.168.255.46"
     response = os.system("ping -c 4 " + ip)
+    #response 0 means success, so if response is 0 everything is good, if not 0, the program should exit
     if response == 0:
         print(ip , 'is reachable')
         print("\n")
-        return ip
     else:
         print(ip , 'is unreachable.')
+        print('Script will exit. Check network connection and re-run script')
+        sys.exit()
 
 def getserial():
     print('Getting Serial info...')
-    #need to add check to make sure the serial actually was grabbed
+    #getting serial info from command below
     cpuinfo=subprocess.Popen("cat /proc/cpuinfo | grep Serial | cut -d' ' -f2", stdout=subprocess.PIPE, shell=True)
     output_bytes, err = cpuinfo.communicate()
+    #Serial is captured initially as bytes and needs to be convereted to string
     output_string = str(output_bytes, 'utf-8')
+    #strip out the trailing line break
     output_string = output_string.strip('\n')
     print('Serial Number is: ' + output_string)
     print("\n")
@@ -33,24 +36,29 @@ def getserial():
     
 def getmacaddr():
     print('Getting MAC Address...')
-    #need to add check to make sure the macaddr actually was grabbed
+    #Mac address is captured by the linux command below
     ifconfig=subprocess.Popen("ifconfig eth0 | grep HWaddr |cut -dH -f2|cut -d\  -f2", stdout=subprocess.PIPE, shell=True)
     if_output_bytes, err = ifconfig.communicate()
+    #Mac convereted from bytes to a string
     if_output_string = str(if_output_bytes, 'utf-8')
+    #strip out the trailing line break
     if_output_string = if_output_string.rstrip('\n')
     print('MAC Address is: ' + if_output_string)
     print("\n")
     return if_output_string
 
-def is_mount(ip):
+def is_mount():
     print('Mounting Network Drive...')
-    print('Checking to make sure network drive is mounted...')
-    #what the mount point should look like
+    #go ahead and run the mount command
     subprocess.Popen("sudo mount -a", stdout=subprocess.PIPE, shell=True)
     time.sleep(5)
-    mount_should="//%s/Folder" % ip
-    mount_point=subprocess.Popen("df | grep Folder | cut -d\  -f1", stdout=subprocess.PIPE, shell=True)
+    print('Checking to make sure network drive is mounted...')
+    #what the mount point should look like
+    mount_should="//192.168.255.46/foldername"
+    #running linux command to get what the actual mount point looks like
+    mount_point=subprocess.Popen("df | grep foldername | cut -d\  -f1", stdout=subprocess.PIPE, shell=True)
     mount_output_bytes, err = mount_point.communicate()
+    #convert bytes to string
     mount_output_string = str(mount_output_bytes, 'utf-8')
     #removes line break after mount point
     mount_output_string = mount_output_string.rstrip('\n')
@@ -72,9 +80,9 @@ def asset_tag():
     #empty check is handlded by the integer input. Empty space is not an integer
     tag_number2 = int(input("Please retype Asset tag number: "))
     if str(tag_number1) == str(tag_number2):
-        #if os.path.isdir("/home/pi/Desktop/SharedFolder/%d" % tag_number1):
-        #    print('Your asset tag number has already been used. Please try again.')
-        #    return asset_tag()
+        if os.path.isdir("/home/pi/Desktop/SharedFolder/%d" % tag_number1):
+            print('Your asset tag number has already been used. Please try again.')
+            return asset_tag()
         tag_number=tag_number1
         print("Asset tag number is %d" % tag_number)
         print("\n")
@@ -123,21 +131,13 @@ def test_code128_barcode_gen(tag_number, serial, macaddr):
     
 
 def main():
-    ip=ping()
-    is_mount(ip)
+    ping()
+    is_mount()
     tag_number=asset_tag()
-#    duplicate_asset_tag(tag_number)
     serial=getserial()
     macaddr=getmacaddr()
     create_text_file(tag_number, serial, macaddr)
     test_code128_barcode_gen(tag_number, serial, macaddr)
     input("Script Finished. Please check the file server to make sure the QA file, and barcodes were generated correctly. Press Enter to exit script.")
 
-
-
 main()
-    
-
-
-
-
